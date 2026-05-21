15343d2cc5f141d9144e9536cb25a59806fa33a3
Ivan Castillo <castillo.ivan.2033@eam.edu.co>
2026-05-20 22:02:58 -0500

    segundo commit

--- app/guardar_ganador.php
+++ app/guardar_ganador.php
@@ -2,27 +2,37 @@
 
 include('../conexion/conexion.php');
 
-//Registrar un nuevo ganador
 if($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $nombre = $_POST['nombre'];
-    $pokemon_id = $_POST['pokemon'];
+
+    $nombre = $_POST['nombre'] ?? '';
+    $pokemon = $_POST['pokemon'] ?? '';
+
+    if(empty($nombre) || empty($pokemon)){
+        die("Datos incompletos");
+    }
+
+    if(!$conn){
+        die("Conexion fallida");
+    }
 
     $query = "INSERT INTO ganadores
     (nombre, pokemon)
     VALUES
-    ($1, $2)";  
+    ($1, $2)";
 
     $resultado = pg_query_params(
         $conn,
         $query,
-        array($nombre, $pokemon_id)
-    );  
+        array($nombre, $pokemon)
+    );
 
     if ($resultado) {
         echo "Ganador registrado exitosamente.";
     } else {
-        echo "Error al registrar el ganador: " . pg_last_error($conn);
+        echo "Error SQL: " . pg_last_error($conn);
     }
+
 } else {
     echo "Acceso invalido";
 }
+?>
--- dockerfile
+++ dockerfile
@@ -1,10 +1,8 @@
-FROM php:8.2-cli
+FROM php:8.2-apache
 
 RUN apt-get update && apt-get install -y libpq-dev \
     && docker-php-ext-install pgsql pdo_pgsql
 
-WORKDIR /app
+COPY . /var/www/html/
 
-COPY . .
-
-CMD ["php", "-S", "0.0.0.0:8000"]
+EXPOSE 80
