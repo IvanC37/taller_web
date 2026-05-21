59c5fd426ef59d08e09a888fded2eccfa1458315
Ivan Castillo <castillo.ivan.2033@eam.edu.co>
2026-05-20 22:33:18 -0500

    reset

--- app/guardar_ganador.php
+++ app/guardar_ganador.php
@@ -1,14 +1,38 @@
 <?php
 
-ini_set('display_errors', 1);
-error_reporting(E_ALL);
-
 include('../conexion/conexion.php');
 
-if($conn){
-    echo "Conexion exitosa";
-} else {
-    echo "Conexion fallida";
-}
+if($_SERVER['REQUEST_METHOD'] === 'POST') {
 
+    $nombre = $_POST['nombre'] ?? '';
+    $pokemon = $_POST['pokemon'] ?? '';
+
+    if(empty($nombre) || empty($pokemon)){
+        die("Datos incompletos");
+    }
+
+    if(!$conn){
+        die("Conexion fallida");
+    }
+
+    $query = "INSERT INTO ganadores
+    (nombre, pokemon)
+    VALUES
+    ($1, $2)";
+
+    $resultado = pg_query_params(
+        $conn,
+        $query,
+        array($nombre, $pokemon)
+    );
+
+    if ($resultado) {
+        echo "Ganador registrado exitosamente.";
+    } else {
+        echo "Error SQL: " . pg_last_error($conn);
+    }
+
+} else {
+    echo "Acceso invalido";
+}
 ?>
